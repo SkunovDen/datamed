2a7757526fc5797f589a77f92951b5e49122a997
den <d.skunov@gmail.com>
2022-11-09 12:01:17 +0300

    with hashrouter and sinple tests

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,39 +1,40 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders DataMed text', () => {
   render(<App />);
 
-  const textElement = screen.getByText('Home');
-  expect(1===1).toBeTruthy();
+  const textElement = screen.getByText('DataMed');
+  expect(textElement).toBeInTheDocument();
 });
 
 
-// test('renders NavLinks', () => {
-//   render(<App />);
+test('renders NavLinks', () => {
+  render(<App />);
 
-//   const homeNavLinkElement = screen.getByText(/Home/i);
-//   expect(homeNavLinkElement).toBeInTheDocument();
+  const homeNavLinkElement = screen.getByText(/Home/i);
+  expect(homeNavLinkElement).toBeInTheDocument();
 
-//   const dataInputNavLinkElement = screen.getByText(/Data Input/i);
-//   expect(dataInputNavLinkElement).toBeInTheDocument();
+  const dataInputNavLinkElement = screen.getByText(/Data Input/i);
+  expect(dataInputNavLinkElement).toBeInTheDocument();
 
-//   const tableViewNavLinkElement = screen.getByText(/Table View/i);
-//   expect(tableViewNavLinkElement).toBeInTheDocument();
+  const tableViewNavLinkElement = screen.getByText(/Table View/i);
+  expect(tableViewNavLinkElement).toBeInTheDocument();
 
-//   const htmlTableNavLinkElement = screen.getByText('***');
-//   expect(htmlTableNavLinkElement).toBeInTheDocument();
-// });
+  const htmlTableNavLinkElement = screen.getByText('***');
+  expect(htmlTableNavLinkElement).toBeInTheDocument();
+});
 
-// test('Nav to Data Input page', () => {
-//   render(<App />);
-//   // clic nav link to Data Input page
-//   const dataInputNavLinkElement = screen.getByText(/Data Input/i);
-//   userEvent.click(dataInputNavLinkElement)
+test('Nav to Data Input page', () => {
+  render(<App />);
+  // clic nav link to Data Input page
+  const dataInputNavLinkElement = screen.getByText(/Data Input/i);
+  userEvent.click(dataInputNavLinkElement)
 
-//   // find Upload button
-//   const uploadButtonElement = screen.getByText('Upload file');
-//   expect(uploadButtonElement).toBeInTheDocument();
+  // find Upload button
+  const uploadButtonElement = screen.getByText('Upload file');
+  expect(uploadButtonElement).toBeInTheDocument();
 
-// })
+})
