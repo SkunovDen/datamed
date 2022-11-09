02eda600b12d56ddda6a035c94b7b6ad5bdb3f88
den <d.skunov@gmail.com>
2022-11-09 12:14:41 +0300

    with hashrouter and simple tests

--- src/components/nav/TopMenu.jsx
+++ src/components/nav/TopMenu.jsx
@@ -6,7 +6,7 @@ const TopMenu = () => {
     return(
         <Navbar bg="primary" variant="dark">
             <Container>
-            <Navbar.Brand href="datamed#/">DataMed</Navbar.Brand>
+            <Navbar.Brand href="/">DataMed</Navbar.Brand>
             <Nav className="me-auto">
                 <NavLink to="/" style={{color: "#FFFFFF", margin: "0 10px"}}>Home</NavLink>
                 <NavLink to="/input" style={{color: "#FFFFFF", margin: "0 10px"}}>Data Input</NavLink>
