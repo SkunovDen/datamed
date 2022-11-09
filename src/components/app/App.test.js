cc6edb5ef00923745a43f498017955bb039272a5
den <d.skunov@gmail.com>
2022-11-09 12:31:05 +0300

    with hashrouter and simple tests 2

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -26,15 +26,3 @@ test('renders NavLinks', () => {
   const htmlTableNavLinkElement = screen.getByText('***');
   expect(htmlTableNavLinkElement).toBeInTheDocument();
 });
-
-test('Nav to Data Input page', () => {
-  render(<App />);
-  // clic nav link to Data Input page
-  const dataInputNavLinkElement = screen.getByText(/Data Input/i);
-  userEvent.click(dataInputNavLinkElement)
-
-  // find Upload button
-  const uploadButtonElement = screen.getByText('Upload file');
-  expect(uploadButtonElement).toBeInTheDocument();
-
-})
