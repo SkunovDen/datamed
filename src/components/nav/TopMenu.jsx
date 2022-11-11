b51ce32b156803ac21926ccad7629b1d154b14d9
den <d.skunov@gmail.com>
2022-11-11 13:02:38 +0300

    double dash fail test

--- src/components/nav/TopMenu.jsx
+++ src/components/nav/TopMenu.jsx
@@ -6,7 +6,7 @@ const TopMenu = () => {
     return(
         <Navbar bg="primary" variant="dark">
             <Container>
-            <Navbar.Brand href="datamed/">DataMed</Navbar.Brand>
+            <Navbar.Brand href="datamed/">Data--Med</Navbar.Brand>
             <Nav className="me-auto">
                 <NavLink to="/" style={{color: "#FFFFFF", margin: "0 10px"}}>Home</NavLink>
                 <NavLink to="/input" style={{color: "#FFFFFF", margin: "0 10px"}}>Data Input</NavLink>
